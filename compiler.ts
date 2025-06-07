e15b1b737a0fd1d70c7c919b060dc2a564edca76
arthur <arthur.oceanus@gmail.com>
2025-06-07 16:58:13 +0800

    feat(compiler): extend with options

--- compiler.ts
+++ compiler.ts
@@ -1,6 +1,27 @@
+import { inspect } from "node:util";
 import lexer from "./lexer/index.js";
+import parser from "./parser/index.js";
+import babelParser from "./parser/babel-parser/index.js";
 
-const compiler = (sourceCode: string) => {
-  const tokens = lexer(sourceCode);
-  console.log("lexer tokens: ", tokens);
+type Options = {
+  parser?: "babel";
 };
+
+const compiler = (sourceCode: string, options: Options = {}) => {
+  const tokens = lexer(sourceCode);
+  const ast = options.parser === "babel" ? babelParser(tokens) : parser(tokens);
+  console.log("\n========= lexer tokens =========");
+  console.log(tokens);
+  console.log(
+    typeof options.parser === "undefined"
+      ? "\n========= parser ast ========="
+      : `\n========= ${options.parser} parser ast =========`,
+  );
+  console.log(inspect(ast, { depth: null, colors: true }));
+};
+
+compiler(
+  `
+const x = test();
+`,
+);
